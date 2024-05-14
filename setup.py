1c9ce15fc0e2e27dfd5af4ee9aee1d9fccba7a48
v1shal-k-l <vishalkumarlal44@gmail.com>
2024-05-15 02:23:21 +0530

    after adding e

---
+++ setup.py
@@ -0,0 +1,19 @@
+## to get info about packages 
+from setuptools import find_packages ,setup
+from typing import List
+
+
+def get_requirements()->list[str]:
+
+    requirements_list=list[str]=[]
+
+    return requirements_list
+setup(
+    name="Sensor",
+    version="0.0.1",
+    author="Vishal",
+    author_email="vishalkumarlal44@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+
+)
